5d9d05e6217224034ca0287de0c07f4da8286a94
Arunas Dzedulionis <36657198+adzedas63@users.noreply.github.com>
2020-11-28 16:58:59 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,27 +1,21 @@
-$(function(){
+$(function () {
 
-$('.top-slider').slick({
+
+
+  $('.top-slider').slick({
     dots: true,
     slidesToShow: 1,
     slidesToScroll: 1,
     nextArrow: '<button type="button" class="slick-arrow slick-next"><img src="images/arrows_right.svg" alt="next arrow"></button>',
     prevArrow: '<button type="button" class="slick-arrow slick-prev"><img src="images/arrows_left.svg" alt="prev arrow"></button>',
-    responsive: [
-      {
+    responsive: [{
         breakpoint: 1024,
         settings: {
-         
+
         }
       },
-     
-      {
-        breakpoint: 640,
-        settings: {
-          arrows: false,
-        }
-      }
     ]
   });
-          
+
 });
 
